bb75772157299f412fbc3398a8ec57594df1d795
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-07 00:42:01 -0300

    Fix : Scroll bar appearance & Code refactor

--- src/organisms/Header/styles.ts
+++ src/organisms/Header/styles.ts
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-  margin-top: 1.875rem;
-  height: 2.625rem;
+  padding-top: 1.875rem;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
--- src/templates/Frontend/index.tsx
+++ src/templates/Frontend/index.tsx
@@ -11,6 +11,7 @@ function Frontend({ children }: FrontendProps) {
   return (
     <S.Container>
       <Header></Header>
+      {/* <main>{children}</main> */}
     </S.Container>
   );
 }
